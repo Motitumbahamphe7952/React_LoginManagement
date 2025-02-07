2899748680c5bfecd82fb5aa45ce1e41689f2108
Nikhil limbu <nikhillimbu918@gmail.com>
2025-02-08 01:36:05 +0545

    Update UpdateSpecificUser.jsx

--- src/components/webusers/UpdateSpecificUser.jsx
+++ src/components/webusers/UpdateSpecificUser.jsx
@@ -183,7 +183,7 @@ const UpdateSpecificUser = () => {
 
       navigate(`/admin/${id}`);
     } catch (error) {
-      toast.error(error.response.data.message);
+      toast.error(error?.response?.data?.message);
     }
   };
 
